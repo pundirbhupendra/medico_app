8f58e863f5a83d2d04b73db89c2559d6a7dec23a
pundirbhupendra <bhupendrapundir@outlook.com>
2020-08-26 21:38:40 +0530

    new update

--- lib/src/home/home_page.dart
+++ lib/src/home/home_page.dart
@@ -4,7 +4,6 @@ import 'package:medico_app/src/presentation/utils/common_toast.dart';
 import 'package:medico_app/src/presentation/widget/common_button.dart';
 import 'package:medico_app/src/res/app_color.dart';
 import 'package:medico_app/src/route/router.gr.dart';
-import 'package:medico_app/src/ui/profile/booking/booking_page.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,16 +11,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  bool _isRadioSelected = false;
-  static const List<String> selections = <String>[
-    'Follow Up',
-    'Consultation',
-  ];
-  int groupValue;
-  static const List<String> daySlot = <String>["1", "2", "3"];
-   static const List<String> timeSlot = <String>["5:30", "6:00", "7:00"];
-  int selecteDayIndex;
-  int selecteTimeIndex;
+ 
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,94 +20,27 @@ class _HomePageState extends State<HomePage> {
           //  backgroundColor: Colors.white,
           elevation: 0.0,
         ),
-        body: ListView(shrinkWrap: true, children: [
-          ListTile(
-            leading: CircleAvatar(),
-            title: Text('Dr.Alvin'),
-            subtitle: Text('The...'),
-          ),
-          Divider(),
-          Text('Purpose of Visit'),
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            children: List.generate(selections.length, (index) {
-              return RadioListTile(
-                  dense: true,
-                  controlAffinity: ListTileControlAffinity.trailing,
-                  value: index,
-                  title: Text(
-                    selections[index],
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  groupValue: groupValue,
-                  onChanged: (v) {
-                    setState(() {
-                      groupValue = v;
-                    });
-                  });
-            }).toList(),
-          ),
-          Text('Day of Slot'),
-          Wrap(
-            direction: Axis.horizontal,
-            spacing: 30,
-            children: List.generate(daySlot.length, (index) {
-              return ChoiceChip(
-                  selectedColor: Colors.blue,
-                  onSelected: (v) {
-                    setState(() {
-                      selecteDayIndex = v ? index : 0;
-                    });
-                    print(v);
-                  },
-                  label: Text(daySlot[index]),
-                  selected: selecteDayIndex == index);
-            }).toList(),
-          ),
-           Text('Time of Slot'),
-               Text('Evening'),
+        body: 
+            Column(
+              children: [
+                Center(
+                  child: Text('Welcome to Medico Home'),
+                ),
+                RaisedButton(
+                    child: Text('Logout'),
+                    onPressed: () async {
 
-               Wrap(
-            direction: Axis.horizontal,
-            spacing: 30,
-            children: List.generate(timeSlot.length, (index) {
-              return ChoiceChip(
-                  selectedColor: Colors.blue,
-                  onSelected: (v) {
-                    setState(() {
-                      selecteTimeIndex = v ? index : 0;
-                    });
-                    print(v);
-                  },
-                  label: Text(timeSlot[index]),
-                  selected: selecteTimeIndex == index);
-            }).toList(),
-          ),
+                     _singout();
+                    }),
+                    RaisedButton(
+                    child: Text('Profile'),
+                    onPressed: () async {
 
-          CommonButton(
-            buttonColor: AppColors.primaryColor,
-            title: "Proceed")
-        ]
-            // Column(
-            //   children: [
-            //     Center(
-            //       child: Text('Welcome to Medico Home'),
-            //     ),
-            //     RaisedButton(
-            //         child: Text('Logout'),
-            //         onPressed: () async {
-
-            //          _singout();
-            //         }),
-            //         RaisedButton(
-            //         child: Text('Profile'),
-            //         onPressed: () async {
-
-            //         Navigator.pushNamed(context, Router.myProfile);
-            //         })
-            //   ],
-            // ),
-            ));
+                    Navigator.pushNamed(context, Router.myProfile);
+                    })
+              ],
+            ),
+            );
   }
 
   _singout() async {
--- lib/src/ui/profile/booking/booking_page.dart
+++ lib/src/ui/profile/booking/booking_page.dart
@@ -1,27 +1,91 @@
-
 import 'package:flutter/material.dart';
+import 'package:medico_app/src/presentation/widget/common_button.dart';
+import 'package:medico_app/src/res/app_color.dart';
 
-class BookingPage extends StatelessWidget {
+class BookingPage extends StatefulWidget {
+  static const List<String> selections = <String>[
+    'Follow Up',
+    'Consultation',
+  ];
+  static const List<String> daySlot = <String>["1", "2", "3"];
+  static const List<String> timeSlot = <String>["5:30", "6:00", "7:00"];
+
+  @override
+  _BookingPageState createState() => _BookingPageState();
+}
+
+class _BookingPageState extends State<BookingPage> {
+  int groupValue;
+
+  int selecteDayIndex;
+
+  int selecteTimeIndex;
+
   @override
   Widget build(BuildContext context) {
-    return   ListView(
-      shrinkWrap: true,
-     children: [
-     Row(
-       children: [
-         ListTile(
-           leading: CircleAvatar(
-
-         ),
-         title: Text('Dr.Alvin'),
-         subtitle: Text('The...'),
-
-         ),
-         Divider(),
-       ],
-     )
-
-     ],
-    );
+    return ListView(shrinkWrap: true, children: [
+      ListTile(
+        leading: CircleAvatar(),
+        title: Text('Dr.Alvin'),
+        subtitle: Text('The...'),
+      ),
+      Divider(),
+      Text('Purpose of Visit'),
+      Column(
+        mainAxisSize: MainAxisSize.min,
+        children: List.generate(BookingPage.selections.length, (index) {
+          return RadioListTile(
+              dense: true,
+              controlAffinity: ListTileControlAffinity.trailing,
+              value: index,
+              title: Text(
+                BookingPage.selections[index],
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              groupValue: groupValue,
+              onChanged: (v) {
+                setState(() {
+                  groupValue = v;
+                });
+              });
+        }).toList(),
+      ),
+      Text('Day of Slot'),
+      Wrap(
+        direction: Axis.horizontal,
+        spacing: 30,
+        children: List.generate(BookingPage.daySlot.length, (index) {
+          return ChoiceChip(
+              selectedColor: Colors.blue,
+              onSelected: (v) {
+                setState(() {
+                  selecteDayIndex = v ? index : 0;
+                });
+                print(v);
+              },
+              label: Text(BookingPage.daySlot[index]),
+              selected: selecteDayIndex == index);
+        }).toList(),
+      ),
+      Text('Time of Slot'),
+      Text('Evening'),
+      Wrap(
+        direction: Axis.horizontal,
+        spacing: 30,
+        children: List.generate(BookingPage.timeSlot.length, (index) {
+          return ChoiceChip(
+              selectedColor: Colors.blue,
+              onSelected: (v) {
+                setState(() {
+                  selecteTimeIndex = v ? index : 0;
+                });
+                print(v);
+              },
+              label: Text(BookingPage.timeSlot[index]),
+              selected: selecteTimeIndex == index);
+        }).toList(),
+      ),
+      CommonButton(buttonColor: AppColors.primaryColor, title: "Proceed")
+    ]);
   }
 }
